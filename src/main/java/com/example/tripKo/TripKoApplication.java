7638ab10ea92ecca048aa252af4b8a9a6cb607f8
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-11-15 11:57:42 +0900

    fix: disableEc2Metadata 설정

--- src/main/java/com/example/tripKo/TripKoApplication.java
+++ src/main/java/com/example/tripKo/TripKoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class TripKoApplication {
-
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TripKoApplication.class, args);
 	}
