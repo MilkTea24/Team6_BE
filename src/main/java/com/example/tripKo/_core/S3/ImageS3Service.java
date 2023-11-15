22888f83c285bdd9159da7ff7b531f2aa874ff84
manjae98 <manjae98@naver.com>
2023-11-15 12:54:38 +0900

    fix: s3 import 추가

--- src/main/java/com/example/tripKo/_core/S3/ImageS3Service.java
+++ src/main/java/com/example/tripKo/_core/S3/ImageS3Service.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.InputStream;
 
 import java.io.File;
 import java.io.IOException;
